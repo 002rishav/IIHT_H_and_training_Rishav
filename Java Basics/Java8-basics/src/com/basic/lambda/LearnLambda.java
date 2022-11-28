abf73174b45714480fbdbfb2834f5dd1c38899fa
unknown <cogjava4585@iiht.tech>
2022-11-28 15:21:30 +0530

    added the lambda file

---
+++ Java Basics/Java8-basics/src/com/basic/lambda/LearnLambda.java
@@ -0,0 +1,30 @@
+package com.basic.lambda;
+
+public class LearnLambda {
+	
+	
+	//parameter -> expression body
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		
+		LearnLambda learn=new LearnLambda();
+		
+		MathOperation add= (Integer num1,Integer num2)-> num1+num2;
+		
+		MathOperation sub= (Integer num1,Integer num2)-> num1-num2;
+		
+		MathOperation mul= (Integer num1,Integer num2)-> num1*num2;
+		
+		MathOperation div= (Integer num1,Integer num2)-> num1/num2;
+		
+		System.out.println("addition of numbers 100,50: "+ add.Operation(100,50));
+		System.out.println("subtraction of numbers 100,50: "+sub.Operation(100, 50));
+		System.out.println("multiplication of numbers 100,50: "+mul.Operation(100, 50));
+		System.out.println("division of numbers 100,50: "+div.Operation(100, 50));
+	}
+	
+	
+	
+
+}
---
+++ Java Basics/Java8-basics/src/com/basic/lambda/MathOperation.java
@@ -0,0 +1,6 @@
+package com.basic.lambda;
+
+public interface MathOperation {
+	
+	Integer Operation(Integer num1,Integer num2);
+}
