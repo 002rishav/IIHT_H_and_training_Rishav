d0010068443263298e9fafb1547fba544e72d97a
unknown <cogjava4585@iiht.tech>
2022-11-29 17:47:36 +0530

    update commit 29-11

---
+++ Java Basics/Java8-basics/src/com/basic/dateandtimeapi/DateTime.java
@@ -0,0 +1,30 @@
+package com.basic.dateandtimeapi;
+import java.text.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class DateTime {
+
+	public static void main(String[] args) {
+		
+//		Date today = new Date();
+		LocalDate today = LocalDate.now();
+		//We don't need new because we don't want to create an object
+		
+		System.out.println("Local Date: "+today);
+		
+		LocalDateTime rightNow = LocalDateTime.now();
+		System.out.println("Local Date and Time: "+rightNow);
+		
+		LocalDate rightNow1 = LocalDate.of(2022, Month.AUGUST, 15);
+		System.out.println("Specific Date: "+rightNow1);
+		
+		LocalTime t1 = LocalTime.now();
+		System.out.println("Local Time: "+t1);
+		
+
+	}
+
+}
