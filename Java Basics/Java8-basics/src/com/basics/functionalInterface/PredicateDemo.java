b8acb0758f533b374fcf536aa15530382ed648b8
unknown <cogjava4585@iiht.tech>
2022-11-29 15:21:06 +0530

    update commit 29-11

--- Java Basics/Java8-basics/src/com/basics/functionalInterface/PredicateDemo.java
+++ Java Basics/Java8-basics/src/com/basics/functionalInterface/PredicateDemo.java
@@ -9,13 +9,13 @@ public class PredicateDemo /*implements Predicate<Integer>*/ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Predicate<Integer> predicate=new PredicateDemo();
-		Predicate<Integer> predicate=(t)->(t%2==0);
-		System.out.println(predicate.test(6));
+//		Predicate<Integer> predicate=(t)->(t%2==0);
+//		System.out.println(predicate.test(6));
 		
 		List<Integer> numberList = Arrays.asList(1,2,3,4,5);
 		
 		
-		numberList.stream().filter(predicate).forEach(number->System.out.println("printing:" + number));
+		numberList.stream().filter(t->t%2==0).forEach(number->System.out.println("printing:" + number));
 	}
 
 //	@Override
