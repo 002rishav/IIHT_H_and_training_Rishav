3c07667a831d4fb017a2af6e3b02224ff29f5963
unknown <cogjava4585@iiht.tech>
2022-11-29 16:26:51 +0530

    update commit 29-11

--- Java Basics/Java8-basics/src/com/basic/reference/Project.java
+++ Java Basics/Java8-basics/src/com/basic/reference/Project.java
@@ -1,9 +1,17 @@
 package com.basic.reference;
 
+import java.time.LocalDate;
+
 public class Project {
 	
 	public static void projectProgress() {
 		System.out.println("Project is making progress");
+		
+		System.out.println("Project status was approved by client");
+		
+		LocalDate now=LocalDate.now();
+		System.out.println("on "+now.toString());
+		
 	}
 
 }
