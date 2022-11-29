936b3a90352619cf04f622fc38959c34acd41b4f
unknown <cogjava4585@iiht.tech>
2022-11-29 16:20:22 +0530

    update commit 29-11

--- Java Basics/Java8-basics/src/com/basic/reference/RefDemo.java
+++ Java Basics/Java8-basics/src/com/basic/reference/RefDemo.java
@@ -4,6 +4,18 @@ public class RefDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("Method reference demo");
+	
+	
+	//provide implementation of IWork
+	IWork iWork = ()->{
+		System.out.println("This is doProject from IWork");
+		};
+	iWork.doProject();
+	
+	IWork iWork1 = Project::projectProgress;
+	iWork1.doProject();
+	
+	
 	}
 
 }
