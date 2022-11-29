004a9481c13ffcce20b57e6629055747c1522b25
unknown <cogjava4585@iiht.tech>
2022-11-29 16:49:45 +0530

    update commit 29-11

--- Java Basics/Java8-basics/src/com/basic/reference/RefDemo.java
+++ Java Basics/Java8-basics/src/com/basic/reference/RefDemo.java
@@ -7,14 +7,21 @@ public class RefDemo {
 	
 	
 	//provide implementation of IWork
-	IWork iWork = ()->{
-		System.out.println("This is doProject from IWork");
-		};
-	iWork.doProject();
+//	IWork iWork = ()->{
+//		System.out.println("This is doProject from IWork");
+//		};
+//	iWork.doProject();
 	
 	IWork iWork1 = Project::projectProgress;
 	iWork1.doProject();
+	 
+//	Runnable runnable = Project::threadTask;
+//	Thread t1 = new Thread(runnable);
+//	t1.start();
 	
+	Runnable runnable1 = Project::oddto100;
+	Thread t2=new Thread(runnable1);
+	t2.start();
 	
 	}
 
