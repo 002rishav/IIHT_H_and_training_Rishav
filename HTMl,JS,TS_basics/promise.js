aaf0a04e887f1c54974db39ea455c527d402d2f9
unknown <cogjava4585@iiht.tech>
2022-12-01 16:56:23 +0530

    promises,asynce wait

--- HTMl,JS,TS_basics/promise.js
+++ HTMl,JS,TS_basics/promise.js
@@ -27,17 +27,17 @@ const fun2 = ()=>{
     return new Promise((resolve,reject) => {
         setTimeout(()=>{
             resolve("This is Function 2");
-            },1000);
+            },2000);
     });
 };
 const fun3 = ()=>{
     return "This is Function 3";
 };
 
-const callFunc = ()=>{
+const callFunc = async ()=>{
     let val1=fun1();
     console.log(val1);
-    let val2=fun2();
+    let val2=await fun2();
     console.log(val2);
     let val3=fun3();
     console.log(val3);
