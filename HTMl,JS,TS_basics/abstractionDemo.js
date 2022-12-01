2d45686e7d0624917afc0d4218adb146bacbe243
unknown <cogjava4585@iiht.tech>
2022-12-01 17:59:04 +0530

    added abstraction

---
+++ HTMl,JS,TS_basics/abstractionDemo.js
@@ -0,0 +1,24 @@
+function Car(color, model,cost,fuelType){
+    this.color=color;
+    this.model=model;
+    this.cost=cost;
+    this.fuelType=fuelType;
+    let runningCostPerYear= 30000;
+
+    this.getCarDetails = function() {
+        console.log("Color: "+this.color);
+        console.log("Model: "+this.model);
+        console.log("Cost: "+this.cost);
+        console.log("fuelType: "+this.fuelType);
+        console.log("Running cost of car per Year: "+this.runningCostPerYear);
+    }
+
+    this.totalCostOfOwnership= function(){
+        console.log(runningCostPerYear + cost);
+    }
+}
+
+let MercedesCclass = new Car("Red","C class",5000000,"Petrol");
+MercedesCclass.getCarDetails();
+MercedesCclass.runningCostPerYear=90000;
+console.log(MercedesCclass.totalCostOfOwnership());
