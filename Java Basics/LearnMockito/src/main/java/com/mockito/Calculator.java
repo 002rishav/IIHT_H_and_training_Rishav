f17576dfd69503c34f6c07f0bc0b79a68e7f909a
unknown <cogjava4585@iiht.tech>
2022-12-02 17:42:53 +0530

    added mockito

--- Java Basics/LearnMockito/src/main/java/com/mockito/Calculator.java
+++ Java Basics/LearnMockito/src/main/java/com/mockito/Calculator.java
@@ -2,14 +2,28 @@ package com.mockito;
 
 public class Calculator {
 
-	public Integer add(Integer num1,Integer num2) {
+	CalculatorService service;
+	public Calculator(CalculatorService service) {
+		this.service=service;
+	}
+	public int add(Integer num1,Integer num2) {
 		return num1+num2;
 	}
-	public Integer sub(Integer num1,Integer num2) {
+	public int sub(Integer num1,Integer num2) {
 		return num1-num2;
 	}
-	public Integer mul(Integer num1,Integer num2) {
+	public int mul(Integer num1,Integer num2) {
 		return num1*num2;
+	}
+	
+	public int perform(int i, int j) {
+		return (i+j);
+	}
+	public int perform1(int i, int j) {
+		return (i-j);
+	}
+	public int perform2(int i, int j) {
+		return (i*j);
 	}
 	
 }
--- Java Basics/LearnMockito/src/test/java/com/mockito/TestCalculator.java
+++ Java Basics/LearnMockito/src/test/java/com/mockito/TestCalculator.java
@@ -1,16 +1,45 @@
 package com.mockito;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import junit.framework.TestCase;
 
 public class TestCalculator {
 	
-	Calculator c =new Calculator();
+	Calculator c =null;
+//	CalculatorService service= new CalculatorService() {		
+//		public int add(int num1, int num2) {
+//			return 0;
+//		}
+//	};
+	CalculatorService service = Mockito.mock(CalculatorService.class);
+	
+	@Before
+	public void setup() {
+		c =new Calculator(service);
+	}
 	
 	@Test
 	public void testAdd() {
-		
+		when(service.add(110, 40)).thenReturn(150);
+		assertEquals(150,c.perform(110,40));
+	}
+	
+	@Test
+	public void testSubtract() {
+		when(service.sub(110, 40)).thenReturn(70);
+		assertEquals(100,c.sub(150,50));
+	}
+	
+	@Test
+	public void testMultiplication() {
+		when(service.mul(100, 40)).thenReturn(4000);
+		assertEquals(4000,c.mul(100,40));
 	}
 
 }
