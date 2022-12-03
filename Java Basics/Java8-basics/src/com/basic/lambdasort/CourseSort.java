1af6068a4680f6311887d07c8a928163b2f8b8f5
unknown <cogjava4585@iiht.tech>
2022-12-03 08:32:42 +0530

    Lambda-sort Complete

--- Java Basics/Java8-basics/src/com/basic/lambdasort/CourseSort.java
+++ Java Basics/Java8-basics/src/com/basic/lambdasort/CourseSort.java
@@ -3,33 +3,48 @@ package com.basic.lambdasort;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CourseSort {
 
-	public static void printDetailsUsingFeesCondition(){
-        System.out.println("printDetails Using FeesCondition : ");
-        List<Courses> courseList = CourseData.getAllCourses();
-        List<Courses> ab=courseList.stream()
-        	.sorted(Comparator.comparing(p -> p.getTotal_fees()))
+	static List<Courses> courseList = CourseData.getAllCourses();
+	
+	public static void printDetailsUsingFeesAscending(){
+        System.out.println("printDetails Using FeesAscendingOrcer : ");
+//        List<Courses> courseList = CourseData.getAllCourses();
+        
+        List<Courses> fees_ascending=courseList.stream()
+        	.sorted(Comparator.comparing(c -> c.getTotal_fees()))
         	.collect(Collectors.toList());
-        System.out.println(ab);
+        System.out.println(fees_ascending);
+    }
+	
+	public static void printDetailsUsingFeesDescending(){
+        System.out.println("\nprintDetails Using FeesDescendingOrder : ");
+
+        List<Courses> fees_descending=courseList.stream()
+        	.sorted(Comparator.comparing(c -> c.getTotal_fees()))
+        	.collect(Collectors.toList());
+        Collections.reverse(fees_descending);
+        System.out.println(fees_descending);
     }
 	
 	public static void printDetailsUsingDurationCondition(){
         System.out.println("\nprintDetails Using DurationCondition : ");
-        List<Courses> courseList = CourseData.getAllCourses();
-        List<Courses> ab=courseList.stream()
-        	.sorted(Comparator.comparing(p -> p.getCourse_duration()))
+        
+        List<Courses> duration=courseList.stream()
+        	.sorted(Comparator.comparing(c -> c.getCourse_duration()))
         	.collect(Collectors.toList());
-        System.out.println(ab);
+        System.out.println(duration);
     }
 	
 	public static void main(String[] args) {
 		
-		printDetailsUsingFeesCondition();
+		printDetailsUsingFeesAscending();
+		printDetailsUsingFeesDescending();
 		printDetailsUsingDurationCondition();
 
 	}
