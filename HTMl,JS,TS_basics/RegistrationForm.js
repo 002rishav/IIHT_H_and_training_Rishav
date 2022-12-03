8a15b3f9cfc8bd85ea7680cd6392b98b965a5497
unknown <cogjava4585@iiht.tech>
2022-12-03 11:38:18 +0530

    Registration form

---
+++ HTMl,JS,TS_basics/RegistrationForm.js
@@ -0,0 +1,28 @@
+function display(){
+    let name=document.getElementById("fullname").value;
+    console.log("Name: "+name);
+    let dob=document.getElementById("dob").value;
+    console.log("DOB: "+dob);
+    let email=document.getElementById("email").value;
+    console.log("Email: "+email);
+    let mobile=document.getElementById("mobile").value;
+    console.log("Mobile: "+mobile);
+    let gender=document.getElementById("gender").value;
+    console.log("Gender: "+gender);
+    let occupation=document.getElementById("occupation").value;
+    console.log("Occupation: "+occupation);
+    let id_type=document.getElementById("id_type").value;
+    console.log("ID Type: "+id_type);
+    let id_num=document.getElementById("id_num").value;
+    console.log("ID Number: "+id_num);
+    let issue_auth=document.getElementById("issue_auth").value;
+    console.log("Issue Authority: "+issue_auth);
+    let issue_date=document.getElementById("issue_date").value;
+    console.log("Issue Date: "+issue_date);
+    let issue_state=document.getElementById("issue_state").value;
+    console.log("Issue State: "+issue_state);
+    let expiry_date=document.getElementById("expiry_date").value;
+    console.log("Expiry Date: "+expiry_date);
+
+    location.href="thanks.html";
+}
