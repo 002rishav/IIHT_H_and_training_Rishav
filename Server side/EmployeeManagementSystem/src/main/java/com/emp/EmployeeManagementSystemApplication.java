19977f85130d72d98007639f9bd2cd467a190c1d
unknown <cogjava4585@iiht.tech>
2022-12-06 16:46:40 +0530

    added the service,service impl,repo,controller

--- Server side/EmployeeManagementSystem/src/main/java/com/emp/EmployeeManagementSystemApplication.java
+++ Server side/EmployeeManagementSystem/src/main/java/com/emp/EmployeeManagementSystemApplication.java
@@ -2,21 +2,20 @@ package com.emp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello World!";
-		
-	}
+//	@GetMapping("/hello")
+//	public String hello(){
+//		return "Hello World!";
+//		
+//	}
 
 }
--- Server side/EmployeeManagementSystem/src/main/java/com/emp/Entity/Employee.java
+++ Server side/EmployeeManagementSystem/src/main/java/com/emp/Entity/Employee.java
@@ -1,7 +1,15 @@
 package com.emp.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	private String ename;
 	private String eposition;
