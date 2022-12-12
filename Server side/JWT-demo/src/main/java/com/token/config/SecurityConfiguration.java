31e5ab21b21276e0f465243872e4115c2cff35c5
unknown <cogjava4585@iiht.tech>
2022-12-12 16:12:05 +0530

    added the jwt token validation

--- Server side/JWT-demo/src/main/java/com/token/config/SecurityConfiguration.java
+++ Server side/JWT-demo/src/main/java/com/token/config/SecurityConfiguration.java
@@ -4,20 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.token.filter.JwtFilter;
 import com.token.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -38,7 +48,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/auth")
 		.permitAll()
 		.anyRequest()
-		.authenticated();
+		.authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
--- Server side/JWT-demo/src/main/java/com/token/utility/JWTUtility.java
+++ Server side/JWT-demo/src/main/java/com/token/utility/JWTUtility.java
@@ -14,55 +14,55 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JWTUtility implements Serializable {
-	
+
 	private static final long serialVersionUID = 23456797236L;
-	
-	public static final long JWT_TOKEN_VALIDITY=5*60*60;
-	
-	private String secretKey="Rishav";
-	
-	//to retrieve username from JWT token
+
+	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+	private String secretKey = "Rishav";
+
+	// to retrieve username from JWT token
 	public String getUsernameFromToken(String token) {
-		return getClaimFromToken(token,Claims::getSubject);
+		return getClaimFromToken(token, Claims::getSubject);
 	}
-	
-	//to retrieve expiration date from token
+
+	// to retrieve expiration date from token
 	public Date getExpirationDateFromToken(String token) {
-		return getClaimFromToken(token,Claims::getExpiration);
+		return getClaimFromToken(token, Claims::getExpiration);
 	}
-	
-	private <T> T getClaimFromToken(String token,Function<Claims,T> claimResolver) {
-		final Claims claims=getAllClaimsFromToken(token);
+
+	private <T> T getClaimFromToken(String token, Function<Claims, T> claimResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
 		return claimResolver.apply(claims);
 	}
-	
-	//for retrieving any information from the token we will need for the secret key
+
+	// for retrieving any information from the token we will need for the secret key
 	private Claims getAllClaimsFromToken(String token) {
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
 	}
-	
-	//method to check if token is expired
+
+	// method to check if token is expired
 	private Boolean isTokenExpired(String token) {
 		final Date expiration = getExpirationDateFromToken(token);
 		return expiration.before(new Date());
 	}
-	
-	//generate token for user
+
+	// generate token for user
 	public String generateToken(UserDetails userDetails) {
-		Map<String,Object> claims=new HashMap<>();
-		return doGenerateToken(claims,userDetails.getUsername());
+		Map<String, Object> claims = new HashMap<>();
+		return doGenerateToken(claims, userDetails.getUsername());
 	}
-	
-	private String  doGenerateToken(Map<String,Object> claims,String subject) {
+
+	private String doGenerateToken(Map<String, Object> claims, String subject) {
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis()+JWT_TOKEN_VALIDITY*1000))
-				.signWith(SignatureAlgorithm.HS512,secretKey).compact();
+				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+				.signWith(SignatureAlgorithm.HS512, secretKey).compact();
 	}
-	
-	//validate the token
-	public Boolean validateToken(String token,UserDetails userDetails) {
+
+	// validate the token
+	public Boolean validateToken(String token, UserDetails userDetails) {
 		final String username = getUsernameFromToken(token);
 		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 	}
-	
+
 }
