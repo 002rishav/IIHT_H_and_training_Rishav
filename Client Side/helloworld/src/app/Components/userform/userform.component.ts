90e156a3dea0e7662804082fb3bbef0a123df2a5
unknown <cogjava4585@iiht.tech>
2022-12-15 17:35:15 +0530

    post operation performed on data

--- Client Side/helloworld/src/app/Components/userform/userform.component.ts
+++ Client Side/helloworld/src/app/Components/userform/userform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import User from 'src/app/Entity/user';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-userform',
@@ -13,13 +14,21 @@ export class UserformComponent implements OnInit {
   user: User = new User();
 
   save(){
-    console.log("Hello " + this.user.firstName + ' ' + this.user.lastName);
-    console.log('your email id is ' + this.user.email);
-    console.log('your age is ' + this.user.age + ' and gender is ' + this.user.gender);
+    const observables = this.userService.saveUser(this.user);
+    observables.subscribe(
+      (response:any) => {
+        console.log(response);
+      }, function(error){
+        console.log(error);
+        alert("something went wrong, Please try again!")
+      })
+    // console.log("Hello " + this.user.firstName + ' ' + this.user.lastName);
+    // console.log('your email id is ' + this.user.email);
+    // console.log('your age is ' + this.user.age + ' and gender is ' + this.user.gender);
   }
 
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
--- Client Side/helloworld/src/app/Services/user.service.ts
+++ Client Side/helloworld/src/app/Services/user.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const BASE_URL = "http://localhost:5000/users";
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  saveUser( user:{
+    firstName:String;
+    lastName:String ;
+    age: number;
+    gender:String;
+    email:String;
+  }){
+    return this.http.post(BASE_URL,user);
+  }
 
-
-
-  
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 }
