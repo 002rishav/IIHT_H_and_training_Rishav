94047b059234a310001e4124c7cdbd466fd86742
unknown <cogjava4585@iiht.tech>
2022-12-19 15:14:40 +0530

    added acions,models,reducers and shared enums folder and ts files

--- Client Side/todo/src/app/reducers/todo.reducers.ts
+++ Client Side/todo/src/app/reducers/todo.reducers.ts
@@ -1,3 +1,4 @@
+import { ActionParent } from "../actions/todo.action";
 import { Todo } from "../models/Todo";
 
 
@@ -6,3 +7,9 @@ const initialState: Todo[] = [
     {title: "title 2"},
     {title: "title 3"}
 ];
+
+export function TodoReducer(state = initialState, action: ActionParent){
+    switch(action.type){
+        default: return state;
+    }
+}
