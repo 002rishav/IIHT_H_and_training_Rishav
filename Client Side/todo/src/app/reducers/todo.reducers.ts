e0b3d506ef5ce6c26c4df6bd16db59a8cfccbba0
unknown <cogjava4585@iiht.tech>
2022-12-19 17:22:58 +0530

    todo functionality completed

--- Client Side/todo/src/app/reducers/todo.reducers.ts
+++ Client Side/todo/src/app/reducers/todo.reducers.ts
@@ -1,5 +1,6 @@
 import { ActionParent } from "../actions/todo.action";
 import { Todo } from "../models/Todo";
+import { TodoActionType } from "../shared/todo-action-types.enums";
 
 
 const initialState:Todo [] =[
@@ -11,6 +12,8 @@ const initialState:Todo [] =[
 
 export function TodoReducer(state = initialState, action:ActionParent) {
     switch(action.type) {
-        default:return state;
+        case TodoActionType.Add:
+            return[...state,action.payload]
+            default:return state;
     }
 }
